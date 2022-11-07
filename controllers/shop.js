d037369d37850d77e87bd924b878834154be730c
Michael Manlulu <michaeldmanlulu@gmail.com>
2022-11-08 06:45:36 +0800

    356. adding a checkout page

--- controllers/shop.js
+++ controllers/shop.js
@@ -132,6 +132,31 @@ exports.postCartDeleteProduct = (req, res, next) => {
     });
 };
 
+exports.getCheckout = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then((user) => {
+      const products = user.cart.items;
+      const totalSum = products.reduce(
+        (total, { quantity, productId }) => total + quantity * productId.price,
+        0
+      );
+
+      res.render('shop/checkout', {
+        path: '/checkout',
+        pageTitle: 'Checkout',
+        products: products,
+        totalSum,
+      });
+    })
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
+};
+
 exports.postOrder = (req, res, next) => {
   req.user
     .populate('cart.items.productId')
